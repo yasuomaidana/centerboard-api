b5b33d2834a0b0e0560a9077a476c9bd7bc33c67
unknown <yasuomaidana>
2023-01-11 16:17:33 +0900

    Creating hello world controller

---
+++ src/main/kotlin/com/yasuo/centerboard/controllers/HelloController.kt
@@ -0,0 +1,15 @@
+package com.yasuo.centerboard.controllers
+
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Produces
+
+@Controller("hello")
+class HelloController {
+    @Get
+    @Produces(MediaType.TEXT_PLAIN)
+    fun index():String{
+        return "Hello world"
+    }
+}
