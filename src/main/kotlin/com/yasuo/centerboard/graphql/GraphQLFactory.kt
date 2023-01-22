b606b8f564e33aeb7180f9af85b9e939924f3014
yasuomaidana <yasuomaidana@gmail.com>
2023-01-23 03:01:28 +0900

    Applying dependency injection principle to Fetchers, adding position fetcher to account fetcher

--- src/main/kotlin/com/yasuo/centerboard/graphql/GraphQLFactory.kt
+++ src/main/kotlin/com/yasuo/centerboard/graphql/GraphQLFactory.kt
@@ -11,6 +11,7 @@ import graphql.schema.idl.TypeDefinitionRegistry
 import io.micronaut.context.annotation.Bean
 import io.micronaut.context.annotation.Factory
 import io.micronaut.core.io.ResourceResolver
+import jakarta.inject.Inject
 import jakarta.inject.Singleton
 import java.io.BufferedReader
 import java.io.InputStreamReader
@@ -18,9 +19,14 @@ import java.io.InputStreamReader
 @Factory
 class GraphQLFactory {
 
+    @Inject
+    lateinit var positionFetcher: PositionFetcher
+    @Inject
+    lateinit var accountsFetcher: AccountsFetcher
+
     @Bean
     @Singleton
-    fun graphQL(resourceResolver: ResourceResolver, accountsFetcher: AccountsFetcher):GraphQL{
+    fun graphQL(resourceResolver: ResourceResolver):GraphQL{
         val parser = SchemaParser()
         val generator = SchemaGenerator()
 
@@ -30,8 +36,13 @@ class GraphQLFactory {
                 resourceResolver.getResourceAsStream("classpath:schema.graphqls").get()))))
 
         val runtimeWiring = RuntimeWiring.newRuntimeWiring()
-            .type("Query"){typeWiring -> typeWiring.dataFetcher("accounts",accountsFetcher)
-            }.build()
+            .type("Query"){
+                    typeWiring -> typeWiring.dataFetcher("accounts",accountsFetcher)
+            }
+            .type("Account"){
+                typeWiring -> typeWiring.dataFetcher("positions",positionFetcher)
+            }
+                .build()
         val schema = generator.makeExecutableSchema(typeRegistry,runtimeWiring)
 
         return  GraphQL.newGraphQL(schema).build()
@@ -39,8 +50,23 @@ class GraphQLFactory {
 }
 
 @Singleton
+class PositionFetcher:DataFetcher<List<Position>>{
+    private val positions = mapOf<String,List<Position>>(
+            "1" to listOf(Position("1","APPL","22","1.2")),
+            "2" to listOf(Position("1","AMZ","10","3.2")),
+            "3" to listOf(Position("1","ALF","10","4.2"))
+        )
+    override fun get(environment: DataFetchingEnvironment): List<Position> {
+
+        val account =  environment.getSource<Account>()
+        return positions.getOrDefault(account.id, emptyList())
+    }
+
+}
+
+@Singleton
 class AccountsFetcher:DataFetcher<List<Account>>{
-    override fun get(environment: DataFetchingEnvironment?): List<Account> {
+    override fun get(environment: DataFetchingEnvironment): List<Account> {
         return listOf(
             Account("1","Savings","Hehe I don have savings"),
             Account("2","Expenses","I have a lot"),
