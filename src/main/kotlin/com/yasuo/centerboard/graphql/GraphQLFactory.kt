ec72f81fc714dce2214a5e76d2b794fe44f1339d
yasuomaidana <yasuomaidana@gmail.com>
2023-01-29 02:25:56 +0900

    Applying kotlin graphql generator, now we do need to implement the wiring etc...

--- src/main/kotlin/com/yasuo/centerboard/graphql/GraphQLFactory.kt
+++ src/main/kotlin/com/yasuo/centerboard/graphql/GraphQLFactory.kt
@@ -1,51 +1,33 @@
 package com.yasuo.centerboard.graphql
 
-import com.yasuo.centerboard.accounts.Account
+import com.expediagroup.graphql.generator.SchemaGeneratorConfig
+import com.expediagroup.graphql.generator.TopLevelObject
+import com.expediagroup.graphql.generator.toSchema
+import com.yasuo.centerboard.graphql.schema.Account
+import com.yasuo.centerboard.graphql.schema.AccountQuery
+import com.yasuo.centerboard.graphql.schema.Position
+import com.yasuo.centerboard.repositories.AccountRepository
 import graphql.GraphQL
 import graphql.schema.DataFetcher
 import graphql.schema.DataFetchingEnvironment
-import graphql.schema.idl.RuntimeWiring
-import graphql.schema.idl.SchemaGenerator
-import graphql.schema.idl.SchemaParser
-import graphql.schema.idl.TypeDefinitionRegistry
 import io.micronaut.context.annotation.Bean
 import io.micronaut.context.annotation.Factory
 import io.micronaut.core.io.ResourceResolver
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
-import java.io.BufferedReader
-import java.io.InputStreamReader
 
 @Factory
 class GraphQLFactory {
 
     @Inject
-    lateinit var positionFetcher: PositionFetcher
-    @Inject
-    lateinit var accountsFetcher: AccountsFetcher
+    lateinit var accountRepository: AccountRepository
 
     @Bean
     @Singleton
     fun graphQL(resourceResolver: ResourceResolver):GraphQL{
-        val parser = SchemaParser()
-        val generator = SchemaGenerator()
-
-         val typeRegistry = TypeDefinitionRegistry()
-        typeRegistry.merge(parser.parse(
-            BufferedReader(InputStreamReader(
-                resourceResolver.getResourceAsStream("classpath:schema.graphqls").get()))))
-
-        val runtimeWiring = RuntimeWiring.newRuntimeWiring()
-            .type("Query"){
-                    typeWiring -> typeWiring.dataFetcher("accounts",accountsFetcher)
-            }
-            .type("Account"){
-                typeWiring -> typeWiring.dataFetcher("positions",positionFetcher)
-            }
-                .build()
-        val schema = generator.makeExecutableSchema(typeRegistry,runtimeWiring)
-
-        return  GraphQL.newGraphQL(schema).build()
+        val config =  SchemaGeneratorConfig(supportedPackages = listOf("com.yasuo.centerboard.graphql.schema"))
+        val queries = listOf(TopLevelObject(AccountQuery(accountRepository)))
+        return  GraphQL.newGraphQL(toSchema(config,queries, emptyList())).build()
     }
 }
 
