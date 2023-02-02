e5ba8a33444edac6efadee33822ff7e7c97b5411
yasuomaidana <yasuomaidana@gmail.com>
2023-02-03 01:55:30 +0900

    Enabling redis and adding cache to our micronaut application

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
 
     implementation("com.expediagroup:graphql-kotlin-schema-generator:7.0.0-alpha.0")
 
+    implementation("io.micronaut.redis:micronaut-redis-lettuce")
+    implementation("io.micronaut.cache:micronaut-cache-core")
 }
 
 
