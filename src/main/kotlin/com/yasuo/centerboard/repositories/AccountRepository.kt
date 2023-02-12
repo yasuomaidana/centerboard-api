bc35e82bc19010bc1a8036d07306346dce2acd33
yasuomaidana <yasuomaidana@gmail.com>
2023-02-12 20:37:29 +0900

    Adding positions to repository, this is temporal

--- src/main/kotlin/com/yasuo/centerboard/repositories/AccountRepository.kt
+++ src/main/kotlin/com/yasuo/centerboard/repositories/AccountRepository.kt
@@ -1,16 +1,16 @@
 package com.yasuo.centerboard.repositories
 
 import com.yasuo.centerboard.graphql.schema.Account
-import graphql.schema.DataFetchingEnvironment
+import com.yasuo.centerboard.graphql.schema.Position
 import jakarta.inject.Singleton
 
 @Singleton
 class AccountRepository{
     fun getAccounts(): List<Account> {
         return listOf(
-            Account("1","Savings","Hehe I don have savings"),
-            Account("2","Expenses","I have a lot"),
-            Account("3","Others","Mmmm mmm suspicious")
+            Account("1","Savings","Hehe I don have savings",false,listOf(Position("1","APPL","22","1.2"))),
+            Account("2","Expenses","I have a lot", positions =listOf(Position("2","AMZ","10","3.2")) ),
+            Account("3","Others","Mmmm mmm suspicious", positions =listOf(Position("1","ALF","10","4.2")))
         )
     }
 }
