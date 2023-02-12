04ac89424383d7157acfcf427fdbd8a54d0ae4ba
yasuomaidana <yasuomaidana@gmail.com>
2023-02-12 20:34:30 +0900

    Changing date type to string

--- src/main/kotlin/com/yasuo/centerboard/graphql/schema/GraphQLDefinitions.kt
+++ src/main/kotlin/com/yasuo/centerboard/graphql/schema/GraphQLDefinitions.kt
@@ -1,5 +1,9 @@
 package com.yasuo.centerboard.graphql.schema
 
+import com.yasuo.centerboard.quotes.QuoteService
+import com.yasuo.centerboard.repositories.AccountRepository
+import jakarta.inject.Inject
+import jakarta.inject.Singleton
 import java.util.*
 
 data class Position(
@@ -7,18 +11,44 @@ data class Position(
     val ticker:String,
     val shares: String,
     val price: String,
-    val date: Date = Date()
+    val date: String = Date().toString()
     )
 
 data class Account (val id:String,
                     val name:String,
                     val description:String = "",
-                    val taxable: Boolean = true
+                    val taxable: Boolean = true,
+                    val positions:List<Position>
                     )
 data class Quote(
-    private val symbol: String,
-    private val price:Double,
-    private val companyName:String,
-    private val high:Double,
-    private val low: Double,
-    private val lastUpdate:Long)
+    val symbol: String,
+    val price:Double,
+    val companyName:String,
+    val high:Double,
+    val low: Double,
+    val lastUpdate:Double)
+
+
+@Singleton
+@Suppress("unused")
+class AccountQuery{
+    @Inject
+    lateinit var accountRepository: AccountRepository
+    fun accounts():List<Account>{
+        return accountRepository.getAccounts()
+    }
+}
+
+@Singleton
+@Suppress("unused")
+class QuoteQuery{
+    @Inject
+    lateinit var quoteService: QuoteService
+    fun quotes(): List<Quote>{
+        return quoteService.getQuotes()
+    }
+
+    suspend fun quote(symbol: String):Quote{
+        return quoteService.getQuoteBySymbol(symbol)
+    }
+}
