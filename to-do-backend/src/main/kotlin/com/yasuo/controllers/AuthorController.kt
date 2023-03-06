043debd27cf7437f060a9b73731b7174eb14dc9d
yasuomaidana <yasuomaidana@gmail.com>
2023-03-06 20:59:54 +0900

    Creating example controller

---
+++ to-do-backend/src/main/kotlin/com/yasuo/controllers/AuthorController.kt
@@ -0,0 +1,17 @@
+package com.yasuo.controllers
+
+import com.yasuo.entities.Author
+import com.yasuo.repositories.AuthorRepository
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import jakarta.inject.Inject
+
+@Controller("/author")
+class AuthorController {
+    @Inject
+    lateinit var authorRepository:AuthorRepository
+    @Get
+    fun getAuthors():List<Author>{
+        return authorRepository.findAll() as List<Author>
+    }
+}
