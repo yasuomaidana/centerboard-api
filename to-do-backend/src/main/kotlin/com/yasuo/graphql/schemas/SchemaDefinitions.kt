261b56fd4390f6180db987a679daa054a7ab32f2
yasuomaidana <yasuomaidana@gmail.com>
2023-03-10 17:19:09 +0900

    Creating to do query

--- to-do-backend/src/main/kotlin/com/yasuo/graphql/schemas/SchemaDefinitions.kt
+++ to-do-backend/src/main/kotlin/com/yasuo/graphql/schemas/SchemaDefinitions.kt
@@ -1,6 +1,8 @@
 package com.yasuo.graphql.schemas
 
 import com.expediagroup.graphql.generator.annotations.GraphQLName
+import com.yasuo.services.ToDoService
+import jakarta.inject.Inject
 import jakarta.inject.Singleton
 
 @GraphQLName("Author")
@@ -13,5 +15,15 @@ data class ToDoSchema(val id:Int, val title: String, val completed:Boolean, val 
 class AuthorQuery{
     fun getAuthor():AuthorSchema{
         return AuthorSchema(1,"Hi")
+    }
+}
+
+@Singleton
+class ToDoQuery{
+    @Inject
+    lateinit var  toDoService: ToDoService
+    fun get():Iterable<ToDoSchema>{
+        //toDoService.getAll().map { raw -> ToDoSchema( raw.id.toInt(), raw.title, raw.completed,raw.) }
+        return listOf()
     }
 }
