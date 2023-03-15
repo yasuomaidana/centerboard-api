91c51376da9948b3e73ad922e311745238bae75c
yasuomaidana <yasuomaidana@gmail.com>
2023-03-15 18:02:50 +0900

    Updating ToDoMapper

--- to-do-backend/src/main/kotlin/com/yasuo/mappers/ToDoMapper.kt
+++ to-do-backend/src/main/kotlin/com/yasuo/mappers/ToDoMapper.kt
@@ -1,9 +1,7 @@
 package com.yasuo.mappers
 
 import com.yasuo.entities.ToDo
-import com.yasuo.graphql.schemas.AuthorSchema
 import com.yasuo.graphql.schemas.ToDoSchema
-import com.yasuo.services.AuthorService
 import jakarta.inject.Inject
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
@@ -12,13 +10,7 @@ import org.mapstruct.Mapping
 abstract class ToDoMapper {
 
     @Inject
-    lateinit var authorService: AuthorService
-    @Mapping(target="author", expression ="java(getAuthorFromId(toDo.getAuthorId()))")
+    lateinit var authorMapper: AuthorMapper
+    @Mapping(target="author", expression ="java(authorMapper.fromIdToAuthorSchema((toDo.getAuthorId())))")
     abstract fun convertToToDoSchema(toDo:ToDo):ToDoSchema
-
-    fun getAuthorFromId(authorId: Long): AuthorSchema {
-        val author = authorService.findByIdIn(listOf(authorId)).first()
-        val userName = author.username.orEmpty()
-        return AuthorSchema(authorId.toInt(), userName)
-    }
 }
