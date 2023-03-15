c8858390b5c8ecfac3b6e7cd65e2c1722bf41771
yasuomaidana <yasuomaidana@gmail.com>
2023-03-15 17:31:11 +0900

    Creating ToDoMapper

---
+++ to-do-backend/src/main/kotlin/com/yasuo/mappers/ToDo/ToDoMapper.kt
@@ -0,0 +1,24 @@
+package com.yasuo.mappers.ToDo
+
+import com.yasuo.entities.ToDo
+import com.yasuo.graphql.schemas.AuthorSchema
+import com.yasuo.graphql.schemas.ToDoSchema
+import com.yasuo.services.AuthorService
+import jakarta.inject.Inject
+import org.mapstruct.Mapper
+import org.mapstruct.Mapping
+
+@Mapper(componentModel = "jsr330")
+abstract class ToDoMapper {
+
+    @Inject
+    lateinit var authorService: AuthorService
+    @Mapping(target="author", expression ="java(getAuthorFromId(toDo.getAuthorId()))")
+    abstract fun convertToToDoSchema(toDo:ToDo):ToDoSchema
+
+    fun getAuthorFromId(authorId: Long): AuthorSchema {
+        val author = authorService.findByIdIn(listOf(authorId)).first()
+        val userName = author.username.orEmpty()
+        return AuthorSchema(authorId.toInt(), userName)
+    }
+}
