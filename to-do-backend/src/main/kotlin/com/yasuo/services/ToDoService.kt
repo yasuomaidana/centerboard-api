6759ea2f3e6d2d6fccf55467da8ed3e55ab6eea0
yasuomaidana <yasuomaidana@gmail.com>
2023-03-16 18:10:10 +0900

    Adding method to service

--- to-do-backend/src/main/kotlin/com/yasuo/services/ToDoService.kt
+++ to-do-backend/src/main/kotlin/com/yasuo/services/ToDoService.kt
@@ -1,8 +1,11 @@
 package com.yasuo.services
 
 import com.yasuo.entities.ToDo
+import javax.transaction.Transactional
 
 interface ToDoService {
     fun getAll(): Iterable<ToDo?>
+    @Transactional
+    fun createToDo(title:String, username:String):ToDo
 
 }
--- to-do-backend/src/main/kotlin/com/yasuo/services/ToDoServiceImplementation.kt
+++ to-do-backend/src/main/kotlin/com/yasuo/services/ToDoServiceImplementation.kt
@@ -4,15 +4,20 @@ import com.yasuo.entities.ToDo
 import com.yasuo.repositories.ToDoRepository
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
-import javax.transaction.Transactional
 
 @Singleton
 class ToDoServiceImplementation:ToDoService {
     @Inject
     lateinit var toDoRepository: ToDoRepository
+    @Inject
+    lateinit var authorService: AuthorService
 
-    @Transactional
     override fun getAll(): Iterable<ToDo?> {
         return toDoRepository.findAll()
+    }
+
+    override fun createToDo(title: String, username: String): ToDo {
+        val author = authorService.findOrCreate(username)
+        return toDoRepository.save(ToDo(title,author.id!!))
     }
 }
