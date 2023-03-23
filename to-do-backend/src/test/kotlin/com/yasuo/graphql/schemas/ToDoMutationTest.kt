ae423a30fecf19d132635fa7557edbca63e8477b
yasuomaidana <yasuomaidana@gmail.com>
2023-03-23 17:30:48 +0900

    COUR-687: Finishing test developing

--- to-do-backend/src/test/kotlin/com/yasuo/graphql/schemas/ToDoMutationTest.kt
+++ to-do-backend/src/test/kotlin/com/yasuo/graphql/schemas/ToDoMutationTest.kt
@@ -2,25 +2,41 @@ package com.yasuo.graphql.schemas
 
 import io.micronaut.core.type.Argument
 import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 @MicronautTest
 class ToDoMutationTest(@Client("/") val client: HttpClient){
     @Test
     fun testItWorks() {
-        val query = "{ \"query\": \"{ getToDos {title, author{ username } }}\" }"
 
-        val request: HttpRequest<String> = HttpRequest.POST("/graphql", query)
-        val rsp = client.toBlocking().exchange(request, Argument.of(Map::class.java))
 
-        Assertions.assertEquals(HttpStatus.OK, rsp.status())
-        Assertions.assertNotNull(rsp.body())
-
-        val toDos = rsp.getBody(Map::class.java).get()["data"] as Map<*, *>
+        val todos = allTodos
+        val username = (todos[0]["author"] as Map<String,String>)["username"]
+        assert(todos.isNotEmpty())
+        assert(username!!.any())
     }
+
+    private fun fetch(query: String): HttpResponse<Map<String, Any>> {
+        val request: HttpRequest<String> = HttpRequest.POST("/graphql", query)
+        val response = client.toBlocking().exchange(
+            request, Argument.mapOf(Argument.STRING, Argument.OBJECT_ARGUMENT)
+        )
+        assertEquals(HttpStatus.OK, response.status())
+        Assertions.assertNotNull(response.body())
+        return response
+    }
+
+    private val allTodos:List<Map<String,Any>>
+        get(){
+            val query = "{ \"query\": \"{ toDos {title, author{ username } }}\" }"
+            val response = fetch(query)
+            return (response.body.get()["data"] as Map<*,*>)["toDos"] as List<Map<String, Any>>
+        }
 }
