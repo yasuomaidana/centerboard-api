ae80865aad0a687d8fa690fc256ced27298def69
yasuomaidana <yasuomaidana@gmail.com>
2023-04-02 19:27:49 +0900

    COUR-962: Implementing nested properties

--- to-do-backend/src/main/kotlin/com/yasuo/configuration/TeamConfiguration.kt
+++ to-do-backend/src/main/kotlin/com/yasuo/configuration/TeamConfiguration.kt
@@ -1,5 +1,6 @@
 package com.yasuo.configuration
 
+import io.micronaut.context.annotation.ConfigurationBuilder
 import io.micronaut.context.annotation.ConfigurationProperties
 
 @ConfigurationProperties("team")
@@ -7,4 +8,21 @@ class TeamConfiguration {
     var name: String? = null
     var color: String? = null
     var playerNames: List<String>? = null
+    @ConfigurationBuilder(prefixes = ["with"], configurationPrefix = "team-admin")
+    var teamAdmin = TeamAdmin.Builder()
+}
+
+class TeamAdmin private constructor(
+        val manager: String?,
+        val coach: String?,
+        val president: String?) {
+    data class Builder(
+            var manager: String? = null,
+            var coach: String? = null,
+            var president: String? = null) {
+        fun withManager(manager: String) = apply { this.manager = manager }
+        fun withCoach(coach: String) = apply { this.coach = coach }
+        fun withPresident(president: String) = apply { this.president = president }
+        fun build() = TeamAdmin(manager, coach, president)
+    }
 }
