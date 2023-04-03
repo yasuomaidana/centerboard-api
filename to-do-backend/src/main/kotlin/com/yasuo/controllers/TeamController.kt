b7264d42975c4df2217ef89f52a79382f2166355
yasuomaidana <yasuomaidana@gmail.com>
2023-04-03 15:42:21 +0900

    COUR-962: Adding stadium to controller to see them, I also created an endpoint which can read properties from context

--- to-do-backend/src/main/kotlin/com/yasuo/controllers/TeamController.kt
+++ to-do-backend/src/main/kotlin/com/yasuo/controllers/TeamController.kt
@@ -1,11 +1,20 @@
 package com.yasuo.controllers
 
+import com.yasuo.configuration.StadiumConfiguration
 import com.yasuo.configuration.TeamConfiguration
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.exceptions.NoSuchBeanException
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.QueryValue
+import io.micronaut.inject.qualifiers.Qualifiers
+import jakarta.inject.Inject
+import jakarta.inject.Named
 
 @Controller("team")
-class TeamController(val teamConfiguration: TeamConfiguration) {
+class TeamController(val teamConfiguration: TeamConfiguration, @Named("pnc") val stadiumConfiguration: StadiumConfiguration) {
+    @Inject
+    lateinit var applicationContext: ApplicationContext
     @Get
     fun getTeam():String{
         return teamConfiguration.name!!
@@ -13,5 +22,21 @@ class TeamController(val teamConfiguration: TeamConfiguration) {
     @Get("/all")
     fun getAll():TeamConfiguration{
         return teamConfiguration
+    }
+    @Get("/stadium")
+    fun defaultStadium(): StadiumConfiguration{
+        return stadiumConfiguration
+    }
+
+    @Get("/stadium/{name}")
+    fun stadium(@QueryValue name:String): StadiumConfiguration? {
+        val prop: StadiumConfiguration
+        try {
+             prop = applicationContext.getBean(StadiumConfiguration::class.java,Qualifiers.byName(name))
+        } catch (nsb:NoSuchBeanException){
+            return null
+        }
+
+        return  prop
     }
 }
