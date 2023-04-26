384355b2566f8e88a195ca9605e8fa2b344b9486
yasuomaidana <yasuomaidana@gmail.com>
2023-04-26 22:58:05 +0900

    COUR-965: Implementing refresh scope and fixing warnings in tests

--- to-do-backend/src/main/kotlin/com/yasuo/scopes/RobotController.kt
+++ to-do-backend/src/main/kotlin/com/yasuo/scopes/RobotController.kt
@@ -1,9 +1,11 @@
 package com.yasuo.scopes
 
+import com.yasuo.scopes.refresheable.RefreshableRobot
 import com.yasuo.scopes.singleton.RobotFather
 import com.yasuo.scopes.singleton.RobotMother
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
 import java.util.*
 
 @Controller("/")
@@ -12,7 +14,10 @@ class RobotController(private val father: RobotFather, private val mother: Robot
                       private  val prototypeMother: com.yasuo.scopes.prototype.RobotMother,
                       private val prototypeFather: com.yasuo.scopes.prototype.RobotFather,
                       private val requestFather: com.yasuo.scopes.request_scope.RobotFather,
-                      private val requestMother: com.yasuo.scopes.request_scope.RobotMother
+                      private val requestMother: com.yasuo.scopes.request_scope.RobotMother,
+                      private val refreshableFather: com.yasuo.scopes.refresheable.RobotFather,
+                      private val refreshableMother: com.yasuo.scopes.refresheable.RobotMother,
+                      private val refreshableRobot: RefreshableRobot
                         ) {
 
 
@@ -31,6 +36,17 @@ class RobotController(private val father: RobotFather, private val mother: Robot
     fun requestRobot():List<String>{
 
         return listOf(requestFather.child().serialNumber, requestMother.child().serialNumber)
+    }
+
+    @Get("/refreshable")
+    fun refreshableRobot():List<String>{
+
+        return listOf(refreshableFather.child().serialNumber, refreshableMother.child().serialNumber)
+    }
+
+    @Post("/refresh")
+    fun refreshRobot(){
+        refreshableRobot.refresh()
     }
 
 }
--- to-do-backend/src/test/kotlin/com/yasuo/scopes/RobotControllerTest.kt
+++ to-do-backend/src/test/kotlin/com/yasuo/scopes/RobotControllerTest.kt
@@ -12,32 +12,49 @@ import java.util.*
 class RobotControllerTest(@Client("/") val client: HttpClient){
     @Test
     fun onlyOneInstanceOfTheBeanExistsForSingletonBeans(){
-        val res: HashSet<String>? = (client.toBlocking().retrieve(HttpRequest.GET<Any>("/singleton"),HashSet::class.java) as? HashSet<String>)
+        val res: HashSet<String> = execute_get("/singleton").toHashSet()
 
         assertNotNull(res)
 
-        assertEquals(1, res!!.size,"It has more elements than expected")
+        assertEquals(1, res.size,"It has more elements than expected")
 
     }
 
     @Test
     fun differentInstancesOfTheBeanExistsForPrototype(){
-        val res: HashSet<String>? = (client.toBlocking().retrieve(HttpRequest.GET<Any>("/prototype"),HashSet::class.java) as? HashSet<String>)
-
+        val res: HashSet<String> = execute_get("/prototype").toHashSet()
         assertNotNull(res)
-
-        assertEquals(2, res!!.size,"It has less elements than expected")
+        assertEquals(2, res.size,"It has less elements than expected")
 
     }
 
     @Test
     fun differentInstancesOfTheBeanExistsForRequest(){
-        val res: HashSet<String>? = (client.toBlocking().retrieve(HttpRequest.GET<Any>("/request").header("UUID",UUID.randomUUID().toString()),HashSet::class.java) as? HashSet<String>)
-        val res2: HashSet<String>? = (client.toBlocking().retrieve(HttpRequest.GET<Any>("/request").header("UUID",UUID.randomUUID().toString()),HashSet::class.java) as? HashSet<String>)
+        val res: HashSet<String> = client.toBlocking().retrieve(HttpRequest.GET<Any>("/request").header("UUID",UUID.randomUUID().toString()),List::class.java).map{r -> r.toString()}.toHashSet()
 
         assertNotNull(res)
-        assertEquals(1, res!!.size,"It has more elements than expected")
-        assertNotEquals(res,res2,"The answer is the same")
+        assertEquals(1, res.size,"It has more elements than expected")
+
+        client.toBlocking().retrieve(HttpRequest.GET<Any>("/request").header("UUID",UUID.randomUUID().toString()),List::class.java).forEach{r->res.add(r.toString())}
+        assertEquals(2, res.size,"Less elements than expected")
     }
+
+    @Test
+    fun refreshRobot(){
+        val res: HashSet<String> = execute_get("/refreshable").toHashSet()
+        assertEquals(1, res.size,"It has more elements than expected")
+        execute_get("/refreshable").forEach { r->res.add(r)}
+
+        assertEquals(1, res.size,"It has more elements than expected")
+
+        client.toBlocking().exchange(HttpRequest.POST("/refresh",Any::class.java),Any::class.java)
+        execute_get("/refreshable").forEach { r->res.add(r)}
+        assertEquals(2, res.size,"It has more elements than expected")
+        execute_get("/refreshable").forEach { r->res.add(r)}
+        assertEquals(2, res.size,"It has more elements than expected")
+    }
+
+    private fun execute_get(uri:String) = client.toBlocking().retrieve(HttpRequest.GET<Any>(uri),List::class.java).map { it.toString() }
+
 
 }
