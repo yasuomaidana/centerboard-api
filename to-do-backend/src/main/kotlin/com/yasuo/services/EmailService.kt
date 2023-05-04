1abfaa6b323c89484109fd240f93e508250dbf9a
yasuomaidana <yasuomaidana@gmail.com>
2023-05-04 22:42:12 +0900

    COUR-967: Creating email service

--- to-do-backend/src/main/kotlin/com/yasuo/services/EmailService.kt
+++ to-do-backend/src/main/kotlin/com/yasuo/services/EmailService.kt
@@ -3,7 +3,9 @@ package com.yasuo.services
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-abstract class EmailService() {
-    val LOG: Logger? = LoggerFactory.getLogger(EmailService::class.java)
+abstract class EmailService {
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(EmailService::class.java)
+    }
     abstract fun send(user:String, message: String)
 }
--- to-do-backend/src/main/kotlin/com/yasuo/services/EmailServiceImplementation.kt
+++ to-do-backend/src/main/kotlin/com/yasuo/services/EmailServiceImplementation.kt
@@ -7,6 +7,6 @@ import java.util.*
 @Singleton
 class EmailServiceImplementation: EmailService() {
     override fun send(user: String, message: String) {
-        LOG?.info("Sending email to {} : {} at {}", user, message, SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(Date()))
+        logger.info("Sending email to {} : {} at {}", user, message, SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(Date()))
     }
 }
